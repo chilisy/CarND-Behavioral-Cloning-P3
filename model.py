f0051ee5a0d8b567f28c92eff25cd3864816f588
chili <liheng.lc.chen@gmail.com>
2017-02-07 07:58:24 +0100

    chili: not functional, implementing image-data-fetching

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
-from keras.layers import Dense, Conv2D, Activation, MaxPooling2D, Flatten, Dropout
+from keras.layers.core import Dense, Activation, Flatten, Dropout
 from keras.applications import InceptionV3
+from keras.models import Sequential
 
 import pandas as pd
 import numpy as np
@@ -21,20 +22,35 @@ def load_data_from_log(driving_log):
     return steering, throttle, brake, speed, center, left, right
 
 def Net():
-    model = InceptionV3(include_top=False, weights='imagenet', input_shape=(160, 320, 3))
+    model = Sequential()
+    model.add(InceptionV3(include_top=False, weights='imagenet', input_shape=(160, 320, 3)))
+    model.layers[0].trainable = False
+
     model.add(Dense(1024, activation='relu'))
-    model.add(Dropout)
-    model.add(Dense(2048, ))
+    model.add(Dropout(0.6))
+    model.add(Dense(512))
+    model.add(Dense(48))
 
-
+    model.add(Dense(1))
     model.add(Activation('sigmoid'))
 
     return model
 
 
-folder_data = '../data/'
+folder_data = 'data/'
 steering, throttle, brake, speed, center, left, right = load_data_from_log(folder_data + 'driving_log.csv')
 
-test_image = cv2.imread(folder_data+center[0])
+test_img = cv2.imread(folder_data+center[0])
+img_data = np.zeros_like(test_img)
+img_data = np.expand_dims(img_data, axis=0)
+for img_name in center:
+    img = cv2.imread(folder_data+img_name)
+    img = np.expand_dims(img, axis=0)
+    img_data = np.append(img_data, img, axis=0)
 
-test_image.shape
+
+img_data = img_data[1:len]
+
+
+#model = Net()
+
